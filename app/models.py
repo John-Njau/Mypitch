f78827c4cf258e7ba1f096f1e0f5496f8a349854
john.njau <john.njoroge@student.moringaschool.com>
2022-05-08 21:32:49 +0300

    user, pitch models

---
+++ app/models.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db, login_manager
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    password_hashed = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    
+    
+    @password.setter
+    def password(self, password):
+        self.password_hashed = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hashed, password)
+    
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Pitch(UserMixin, db.Model):
+    __tablename__ = 'pitch'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    pitchtext = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def save_pitch(self):
+        db.session.add()
+        db.session.commit()
+
+    def delete_pitch(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    commenttext = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'), nullable=False)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_comment(self):
+        db.session.add(self)
+        db.session.commit()
