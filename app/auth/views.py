162f37162e859a011aa3dcd4324a659178669711
john.njau <john.njoroge@student.moringaschool.com>
2022-05-10 00:31:50 +0300

    user authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 
 from . import auth
@@ -15,7 +15,9 @@ def signup():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        # mail_message("Welcome to Pitch", "email/welcome", user.email, user=user)
+        
+        mail_message("Welcome to Pitch", "email/welcome", user.email, user=user)
+        
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/signup.html', signupform=form, title=title)
