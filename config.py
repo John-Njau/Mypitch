2990136157fbc694bd30b51153d36c287314286d
john.njau <john.njoroge@student.moringaschool.com>
2022-05-10 07:11:14 +0300

    deploy

--- config.py
+++ config.py
@@ -19,7 +19,19 @@ class DevConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitchdb'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    
 
 
 
