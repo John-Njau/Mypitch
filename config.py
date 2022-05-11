aa8775f2061876286c5c43f1994998b5383d3f4a
john.njau <john.njoroge@student.moringaschool.com>
2022-05-11 08:11:47 +0300

    added tables

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgres://qplimxwduuflyi:b6e1f62b9a7462f2152f7f57ea80155e112adb4b86f986a17e301e187b154d31@ec2-54-172-175-251.compute-1.amazonaws.com:5432/de0el35k0asfhf'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://qplimxwduuflyi:b6e1f62b9a7462f2152f7f57ea80155e112adb4b86f986a17e301e187b154d31@ec2-54-172-175-251.compute-1.amazonaws.com:5432/de0el35k0asfhf'
 
     # UPLOADED_PHOTOS_DEST = 'app/static/photos'
     
@@ -15,14 +15,16 @@ class Config:
     
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://qplimxwduuflyi:b6e1f62b9a7462f2152f7f57ea80155e112adb4b86f986a17e301e187b154d31@ec2-54-172-175-251.compute-1.amazonaws.com:5432/de0el35k0asfhf'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://qplimxwduuflyi:b6e1f62b9a7462f2152f7f57ea80155e112adb4b86f986a17e301e187b154d31@ec2-54-172-175-251.compute-1.amazonaws.com:5432/de0el35k0asfhf'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitchdb'
     # DEBUG = True
 
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-    # uri = os.getenv('DATABASE_URL')
-    # if uri and uri.startswith('postgres://'):
-    #     uri = uri.replace('postgres://', 'postgresql://', 1)
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+        
+    
     # pass
     
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitchdb'.replace("://","ql://",1)
