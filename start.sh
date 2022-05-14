843906b91254d814d69359e4dd80724602e44591
john.njau <john.njoroge@student.moringaschool.com>
2022-05-14 08:13:53 +0300

    untracking config vars

--- start.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY='csrfpitchToken'
-export MAIL_USERNAME='john.njoroge@student.moringaschool.com'
-export MAIL_PASSWORD='Chester01#'
-export DATABASE_URL='postgres://qplimxwduuflyi:b6e1f62b9a7462f2152f7f57ea80155e112adb4b86f986a17e301e187b154d31@ec2-54-172-175-251.compute-1.amazonaws.com:5432/de0el35k0asfhf'
-
-python3.8 manage.py server
